2411a2f85279ba6ce5d52b48756ea01961d29d91
Hasan Hüseyin TAYAP <73137439+DeveloperHHT@users.noreply.github.com>
2022-09-14 11:19:55 +0300

    Add files via upload

---
+++ Train_and_Show.py
@@ -0,0 +1,208 @@
+# Import relevant libraries for the project
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix, classification_report
+plt.style.use('ggplot')
+
+#Dataset#
+
+# Specify directory of train data
+dir_train = 'folderdirectory/train/'
+dir_train_def = dir_train+'def_front/'  # Class label: Defective
+dir_train_ok = dir_train+'ok_front/'    # Class label: OK
+
+# Specify directory of test data
+dir_test = 'folder directory/test/'
+dir_test_def = dir_test+'def_front/'
+dir_test_ok = dir_test+'ok_front/'
+
+
+# Plot samples of defective and non-defective casting
+fig, axes = plt.subplots(1, 2, figsize=(8,4))
+sample_def = plt.imread(dir_train_def+os.listdir(dir_train_def)[0])
+sample_ok = plt.imread(dir_train_ok+os.listdir(dir_train_ok)[0])
+axes[0].imshow(sample_def)
+axes[1].imshow(sample_ok)
+axes[0].set_title('Casting Sample: Defective', loc='left')
+axes[1].set_title('Casting Sample: OK', loc='left')
+axes[0].grid(False)
+axes[1].grid(False)
+plt.show()
+
+
+# Create dataframe of class distribution
+n_train = [len(os.listdir(dir_train_def)), len(os.listdir(dir_train_ok))]
+n_test = [len(os.listdir(dir_test_def)), len(os.listdir(dir_test_ok))]
+dist_df = pd.DataFrame(
+    data=[n_train, n_test],
+    columns=['Defective', 'OK'],
+    index=['Train', 'Test'])
+
+# Visualize class distribution
+ax = dist_df.T.plot(kind='bar', stacked=True, rot=0, figsize=(8,5), colormap='Accent')
+ax.set_title('Class Distribution', loc='left', weight='bold')
+for bar in ax.patches:
+    ax.text(bar.get_x()+bar.get_width()-0.25,
+            bar.get_y()+bar.get_height()/2,
+            int(bar.get_height()),
+            ha='center', va='center', color='white')
+pct_def = 100*dist_df['Defective'].values.sum()/dist_df.values.sum()
+pct_ok = 100-pct_def
+ax.set_xticklabels([f'Class: Defective ({pct_def:.0f}%)',
+                    f'Class: OK ({pct_ok:.0f}%)'], weight='bold')
+plt.show()
+
+
+# Define instances of ImageDataGenerator
+train_gen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+test_gen = ImageDataGenerator(rescale=1./255)
+
+# Specify parameters/arguments for data generation
+img_size, batch_size, rand_seed = (300, 300), 64, 0
+arg_train = {'target_size': img_size,
+             'color_mode': 'grayscale',
+             'classes': {'ok_front': 0,
+                         'def_front': 1},
+             'class_mode': 'binary',
+             'batch_size': batch_size,
+             'seed': rand_seed}
+arg_test = {'target_size': img_size,
+            'color_mode': 'grayscale',
+            'classes': {'ok_front': 0,
+                        'def_front': 1},
+            'class_mode': 'binary',
+            'batch_size': batch_size,
+            'seed': rand_seed,
+            'shuffle': False}
+
+# Generate data by iterating through directories
+train_set = train_gen.flow_from_directory(
+    directory=dir_train, subset='training', **arg_train)
+valid_set = train_gen.flow_from_directory(
+    directory=dir_train, subset='validation', **arg_train)
+test_set = test_gen.flow_from_directory(
+    directory=dir_test, **arg_test)
+
+
+# Model Building
+
+# Define CNN model architecture
+cnn_model = Sequential([
+    # First block
+    Conv2D(32, 3, activation='relu', padding='same', strides=2,
+           input_shape=img_size+(1,)),
+    MaxPooling2D(pool_size=2, strides=2),
+    # Second block
+    Conv2D(64, 3, activation='relu', padding='same', strides=2),
+    MaxPooling2D(pool_size=2, strides=2),
+    # Flatenning
+    Flatten(),
+    # Fully connected layers
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')        # Only 1 output
+])
+
+# Compile model
+cnn_model.compile(
+    optimizer=Adam(learning_rate=0.001),  # Default lr
+    loss='binary_crossentropy',
+    metrics=['accuracy'])
+
+# Display summary of model architecture
+cnn_model.summary()
+
+# Fit model using train set and validation set
+n_epochs = 50
+cnn_model.fit(
+    train_set,
+    validation_data=valid_set,
+    epochs=n_epochs,
+    callbacks=ModelCheckpoint(
+        'CNN_Casting_Inspection.hdf5',
+        save_best_only=True,
+        monitor='val_loss'),
+    verbose=1)
+
+# Plot learning curve from model history
+histo_dict = cnn_model.history.history
+histo_df = pd.DataFrame(histo_dict, index=range(1,n_epochs+1))
+fig, ax = plt.subplots(figsize=(8,5))
+for m in histo_df.columns:
+    ax.plot(histo_df.index, m, data=histo_df)
+ax.set_xlabel('Epoch')
+ax.set_title('Learning Curve', loc='left', weight='bold')
+ax.legend()
+plt.show()
+
+# Load saved model
+best_model = load_model('./CNN_Casting_Inspection.hdf5')
+
+# Make predictions on images in the test set
+y_pred_prob = best_model.predict(test_set, verbose=1)
+y_pred = (y_pred_prob >= 0.5).reshape(-1,)
+y_true = test_set.classes[test_set.index_array]
+
+# Visualize the confusion matrix
+fig, ax = plt.subplots(figsize=(4,3))
+ax = sns.heatmap(confusion_matrix(y_true,y_pred), annot=True,
+                 annot_kws={'size':14, 'weight':'bold'},
+                 fmt='d', cbar=False, cmap='Blues')
+ax.set_xticklabels(['OK', 'Defective'])
+ax.set_yticklabels(['OK', 'Defective'], va='center')
+plt.tick_params(axis='both', labelsize=14, length=0)
+plt.ylabel('Actual', size=14, weight='bold')
+plt.xlabel('Predicted', size=14, weight='bold')
+plt.show()
+
+
+class_map = {0: 'OK', 1: 'Defective'}
+images, labels = next(iter(test_set))
+images = images.reshape(batch_size,*img_size)
+
+fig, axes = plt.subplots(1, 3, figsize=(9, 4))
+fig.suptitle('Prediction on Test Images', y=0.98, weight='bold', size=14)
+for ax, img, label in zip(axes.flat, images, labels):
+    ax.imshow(img, cmap='gray')
+    [[pred_prob]] = best_model.predict(img.reshape(1, *img_size, -1))
+    pred_label = class_map[int(pred_prob>=0.5)]
+    true_label = class_map[label]
+    prob_class = 100*pred_prob if pred_label=='Defective' else 100*(1-pred_prob)
+    ax.set_title(f'Actual: {true_label}', size=12)
+    ax.set_xlabel(f'Predicted: {pred_label} ({prob_class:.2f}%)',
+                  color='g' if pred_label==true_label else 'r')
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.tight_layout()
+plt.show()
+
+
+misclassified = np.nonzero(y_pred != y_true)[0]
+batch_num = misclassified//batch_size
+image_num = misclassified%batch_size
+
+fig, axes = plt.subplots(1, 4, figsize=(12, 4))
+fig.suptitle('Misclassified Test Images', y=0.98, weight='bold', size=14)
+for ax, bnum, inum in zip(axes.flat, batch_num, image_num):
+    images, labels = test_set[bnum]
+    img = images[inum]
+    ax.imshow(img.reshape(*img_size), cmap='gray')
+    [[pred_prob]] = best_model.predict(img.reshape(1, *img_size, -1))
+    pred_label = class_map[int(pred_prob>=0.5)]
+    true_label = class_map[labels[inum]]
+    prob_class = 100*pred_prob if pred_label=='Defective' else 100*(1-pred_prob)
+    ax.set_title(f'Actual: {true_label}', size=12)
+    ax.set_xlabel(f'Predicted: {pred_label} ({prob_class:.2f}%)',
+                  color='g' if pred_label==true_label else 'r')
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.tight_layout()
+plt.show()
+
